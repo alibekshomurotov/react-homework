fff10c48ab7569a0b2a0ce274f7821704c6b0294
Alibek <shomurodovalibek5@gmail.com>
2025-11-21 17:15:15 +0500

    tahrirlasj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, yangi_ism] = useState("Alibek")
+  const [nima , ozgarishi] = useState([
+    {nomi: "Nexia" , rangi:"qora" , yili:"2016" , Id:1},
+    {nomi: "Damas" , rangi:"oq" , yili:"2021" , Id:2},
+    {nomi: "Cobalt" , rangi:"ko'k" , yili:"2023" , Id:3}
+  ])
+
+
+  const ozgardi = () => {
+    ozgarishi   ([
+      {nomi: "Jentra" , rangi:"qora" , yili:"2019" , Id:1},
+      {nomi: "Malibu" , rangi:"oq" , yili:"2018" , Id:2},
+      {nomi: "TAxo" , rangi:"qora" , yili:"2021" , Id:3}
+    ])
+  }
+  const ozgartir = () => {
+    yangi_ism("Someone")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {nima.map((element)=>{
+        return(
+          <h1>{element.nomi} {element.yili}</h1>
+          
+        )
+        
+      })}
+      <h1>{name} Restoroniga xush kelibisiz! </h1>
+      <button onClick={ozgartir}>Tap!</button>
+      <button onClick={ozgardi}>Tap</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/experiment.js
@@ -0,0 +1,11 @@
+import './free.css'
+import React from 'react';
+const Hello = () => {
+    return(
+        <ol>
+            <li>Tajriba</li>
+        </ol>
+    )
+}
+
+export default Hello;
